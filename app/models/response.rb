455c013f8cbaf400c012404e87c618d33a7646e6
Delian Petrov <delian7@gmail.com>
2025-01-24 01:23:35 +0000

    response toplevel comment

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# The Response class represents an individual response to a survey question.
+# It belongs to a survey and validates that the answer is either true or false.
 class Response < ApplicationRecord
   belongs_to :survey
   validates :answer, inclusion: { in: [true, false] }
